351e53b154dd0ee5d9f18ec4a782f6fc49951fbc
Cagan Sevencan <cagansvncn@gmail.com>
2020-06-10 20:57:34 -0700

    First to do line

---
+++ src/app/todo/shared/todo.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {AngularFireDatabase, AngularFireList} from "@angular/fire/database";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  //To complete toDoList items from firebasedb
+  toDoList: AngularFireList<any>
+  constructor(private firebasedb: AngularFireDatabase) {}
+
+  //In order to retrieve all the to do list items into the toDoList variable:
+
+  getToDoItems() {
+    this.toDoList = this.firebasedb.list('items');
+    return this.toDoList;
+  }
+
+  //If we want to add items into firebasedb:
+  addItems(title: string){
+    this.toDoList.push({    //push comes from AngularFireList obj
+      title: title,
+      isChecked: false
+    });
+  }
+
+  setStateOfItem($key: string, flag: boolean){
+    this.toDoList.update($key, {isChecked: flag});
+  }
+
+  removeItem($key: string){
+    this.toDoList.remove($key).then(r => status);
+  }
+
+
+}
